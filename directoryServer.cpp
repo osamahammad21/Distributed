52ae2ba28054faa37346bd41b4f6045cc1e9d7fe
awael <awael@aucegypt.edu>
2019-11-16 03:37:18 +0200

    added csv support for DS using rapidcsv.h, now saves as everything changes,.
    needs minor testing and replys

--- directoryServer.cpp
+++ directoryServer.cpp
@@ -1,56 +1,122 @@
-#include "drectoryServer.h"
-#include "string.h"
+#include "directoryServer.h"
 using namespace std;
 
 directoryServer::directoryServer()
 {
-    infile.open(usersFile);
-    if (infile.fail())
-    {
-        cout<< "Failed to open input file\n";
-        exit(1);
-    }
-    //constructing dictionary 
-    infile.seekg(0, ios::beg);
-    while (!input.eof())
-    {
+	rapidcsv::Document doc(usersFile);
+	int totalUsers = doc.GetRowCount();
 
-        infile >> username;
-        if (username == "") continue;
-            infile >> usersDict[username].password;
-            infile >> usersDict[username].online;
-            infile >> usersDict[username].ip;
-            infile >> usersDict[username].port;
-            infile >> usersDict[username].token;//malhash lazma?
-            infile >> usersDict[username].imageCount;
+	//constructing dictionary from csv
+	for (int i = 0; i<totalUsers;i++)
+	{
+		string username = doc.GetRowName(i);
+		usersDict[username].password = doc.GetCell<string>(i, 0);
+		usersDict[username].online = doc.GetCell<bool>(i, 1);
+		usersDict[username].ip = doc.GetCell<string>(i, 2);
+		usersDict[username].port = doc.GetCell<unsigned int>(i, 3);
+		usersDict[username].token = doc.GetCell<string>(i, 4);
+		usersDict[username].imageCount = doc.GetCell<int>(i, 5);
 
-            for (int i =0; i<usersDict[username].imageCount;i++)
-            {
-                infile >> usersDict[username].imageName;
-                infile >> usersDict[username].imageID;
-            }        
-    }
-    input.close();
+		for (int j = 6; j < 6 + usersDict[username].imageCount * 2; j += 2)
+		{
+			usersDict[username].imageName.push_back(doc.GetCell<string>(i, j));
+			usersDict[username].imageID.push_back(doc.GetCell<string>(i, j+1));
+		}
+	}
 }
+
+directoryServer::~directoryServer()
+{
+}
+
 void directoryServer::login(string &username, string &password, Message* msg, directoryServer* ds)
 {
     bool isAuthenticated = false;
-    if (myCS->authenticate(username, password))
+    if (ds->authenticate(username, password))
     {
-        data newUser;
-        newUser.online = true;
-        newUser.port = msg->getPort();
-        newUser.ip = msg->getSourceIP();
-        newUser.token = username + password;
+		rapidcsv::Document doc(usersFile);
+        data onlineUser;
+		//update struct
+		usersDict[username].online = true;
+		usersDict[username].port = msg->getPort();
+		usersDict[username].ip = msg->getSourceIP();
+		usersDict[username].token = username + password;
         
+		//update file
+		doc.SetCell<bool>("online", username, usersDict[username].online);
+		doc.SetCell<unsigned int>("port", username, usersDict[username].port);
+		doc.SetCell<string>("ip", username, usersDict[username].ip);
+		doc.SetCell<string>("token", username, usersDict[username].token);
+		doc.Save();
 
-        ds->usersDict[username] = newUser;
-        isAuthenticated = true;
+		isAuthenticated = true;
+
     }
+	//send appropriate reply
 }
-bool directoryServer::authenticate(str &username, str &password)
+
+bool directoryServer::authenticate(string &username, string &password)
 {
     if (usersDict.find(username) != usersDict.end())
         return usersDict[username].password == password;
     return false;
 }
+
+void directoryServer::logout(string& username, Message* msg, directoryServer* ds)
+{
+	rapidcsv::Document doc(usersFile);
+
+	//update struct
+	ds->usersDict[username].online = false;
+
+	//update file
+	doc.SetCell<bool>("online", username, usersDict[username].online);
+
+	//send appropriate reply
+}
+void directoryServer::signup(string& username, string& password, Message* msg, directoryServer* ds)
+{
+	bool exists = false;
+	if (ds->usernameExists(username))
+		exists = true;
+	//update struct and file with new user
+	if (!exists) {
+		rapidcsv::Document doc(usersFile);
+		usersDict[username].password = password;
+		doc.SetCell<string>(0, doc.GetRowCount(), password);
+		doc.SetRowName(doc.GetRowCount() - 1, username);
+		doc.Save();
+	}
+	//send appropriate reply
+}
+
+bool directoryServer::usernameExists(string& username)
+{
+	return usersDict.find(username) != usersDict.end();
+}
+
+void directoryServer::uploadimage(string& username, string& imagename, Message* req, directoryServer* ds)
+{
+	rapidcsv::Document doc(usersFile);
+
+	//imageID unique per user assumed
+	string imageID = to_string(usersDict[username].imageCount+1);
+	ds->usersDict[username].imageCount += 1;
+	ds->usersDict[username].imageID.push_back(imageID);
+	ds->usersDict[username].imageName.push_back(imagename);
+	
+	//add to users.csv
+	doc.SetCell<int>("imageCount", username, usersDict[username].imageCount);
+	vector<string>temp = doc.GetRowNames();
+	int row;
+	for (int i = 0; i < temp.size(); i++)
+		if (temp[i] == username)
+			int row = i;
+	doc.SetCell<string>(6+ usersDict[username].imageCount, row, usersDict[username].imageName.back());
+	doc.SetCell<string>(6 + usersDict[username].imageCount+1, row, usersDict[username].imageID.back());
+	doc.Save();
+
+
+}
+
+
--- directoryServer.h
+++ directoryServer.h
@@ -5,43 +5,47 @@
 #include <fstream>
 #include <unordered_map>
 #include <vector>
-#include "string.h"
+#include <string>
 #include "UDPSocket.h"
 #include "Message.h"
+#include "rapidcsv.h"
+
 
 using namespace std;
 
 class directoryServer
 {
     private:
-        unordered_map <string,data> usersDict;
-        string usersFile = "./users.txt";
-        ifstream infile;
-        ofstream outfile;
-        //everything here is stored in users.txt + username
-        struct data 
-        {
-            string password;
-            bool online = false;
-            string ip = "";
-            unsigned int port;
-            string token;
-            vector <string> imageName;
-            vector <string> imageID;
-            int imageCount = imageID.size()
-     
-        }
-        void login(str&, str&, Message* ,CentralServer*);
-        void logout(const str&, Message*, CentralServer*);
-        void signup(const str&, const str&, Message* ,CentralServer*);
-        void uploadimage(const str&, const str&, Message* ,CentralServer*);
-        void requestImages(const str&, Message* ,CentralServer*);
+        string usersFile = "./users.csv";
+        //everything here is stored in users.csv + username
+		struct data
+		{
+			string password;
+			bool online = false;
+			string ip = "";
+			unsigned int port;
+			string token;
+			vector <string> imageName;
+			vector <string> imageID;
+			int imageCount = imageID.size();
+
+		};
+		unordered_map<string, data> usersDict;
+
+        void login(string&, string&, Message* , directoryServer*);
+        void logout(string&, Message*, directoryServer*);
+        void signup(string&, string&, Message* , directoryServer*);
+        void uploadimage(string&, string&, Message* , directoryServer*);
+        //void requestImages(const string&, Message* , directoryServer*);//useless??
+		bool authenticate(string& username, string& password);
+		bool usernameExists(string&);
+
 
     public:
         directoryServer();
         ~directoryServer();
-        void listen();
-        void doOperation(Message *request);
+        void listen();//todo
+        void doOperation(Message *request);//todo???
 };
 
 #endif
